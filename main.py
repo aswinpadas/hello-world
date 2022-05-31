89848d300c44232f94d573b3a5caf06434cf8527
aswin <aswinpdas83@gmail.com>
2022-05-31 13:57:54 +0530

    fix #1

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
+import unittest
+
+
 def print_hi(name):
     print(f'Hello, world, {name}')
 
 
 if __name__ == '__main__':
     print_hi('john')
+
+
+class SimpleTest(unittest.TestCase):
+
+    # Returns True or False.
+    def test(self):
+        self.assertTrue(True)
